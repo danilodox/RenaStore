b2e750ca56b18103cc1f88a9b27a8066e2a7df27
Danilo Medeiros Dantas <danilomedeiros.dox@gmail.com>
2020-07-01 15:27:14 -0300

    up

--- app/src/main/java/com/br/renastore/activity/MainActivity.java
+++ app/src/main/java/com/br/renastore/activity/MainActivity.java
@@ -1,8 +1,11 @@
 package com.br.renastore.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -15,15 +18,18 @@ import android.view.MenuItem;
 import android.widget.Toast;
 
 import com.br.renastore.R;
+import com.br.renastore.helper.ConfiguracaoFirebase;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
-import com.miguelcatalan.materialsearchview.MaterialSearchView;
+import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
+
 
 public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
     private FirebaseAuth autenticacao;
-    private MaterialSearchView searchView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,11 +40,70 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        inicializarComponentes();
+        //configuracao de objetos
+       // autenticacao = ConfiguracaoFirebase.getFirebaseAutenticacao();
+
+      //  inicializarComponentes();
+
+        //configuraBottomNavigationView();
+
+   /*     FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.viewPage, new FeedFragment()).commit();
+*/
 
 
 
     }
+/*
+    /**
+     * Método responsável por tratar eventos de click na BottomNavigation
+     * @param viewEx
+     */
+/*
+    private void habilitarNavegacao(BottomNavigationViewEx viewEx){
+
+        viewEx.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+
+                switch (item.getItemId()){
+                    case R.id.ic_home :
+                        fragmentTransaction.replace(R.id.viewPage, new FeedFragment()).commit();
+                        return true;
+                    case R.id.ic_pesquisa :
+                        fragmentTransaction.replace(R.id.viewPage, new PesquisaFragment()).commit();
+                        return true;
+                    case R.id.ic_postagem :
+                        fragmentTransaction.replace(R.id.viewPage, new PostagemFragment()).commit();
+                        return true;
+                    case R.id.ic_perfil :
+                        fragmentTransaction.replace(R.id.viewPage, new PerfilFragment()).commit();
+                        return true;
+                }
+
+                return false;
+            }
+        });
+    }*/
+
+  /*  private void configuraBottomNavigationView(){
+        BottomNavigationViewEx bottomNavigationItemViewEx = findViewById(R.id.bottomNavigation);
+
+
+        //Fazer configurações iniciais do Bottom Navigation
+        bottomNavigationItemViewEx.enableAnimation(false);
+        bottomNavigationItemViewEx.enableItemShiftingMode(true);
+        bottomNavigationItemViewEx.enableShiftingMode(false);
+        bottomNavigationItemViewEx.setTextVisibility(true);
+
+        //Habilitar navegação
+        habilitarNavegacao( bottomNavigationItemViewEx);
+    }*/
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -47,14 +112,14 @@ public class MainActivity extends AppCompatActivity {
         inflater.inflate(R.menu.menu_toolbar, menu);
 
         //Configurando botao search
-        MenuItem item = menu.findItem(R.id.search);
+     //   MenuItem item = menu.findItem(R.id.search);
 
-        searchView.setMenuItem( item );
+     //   searchView.setMenuItem( item );
 
 
         return super.onCreateOptionsMenu(menu);
     }
-
+/*
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
@@ -78,7 +143,7 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    private void inicializarComponentes(){
+   /* private void inicializarComponentes(){
         searchView = findViewById(R.id.material_search_view);
-    }
+    }*/
 }
